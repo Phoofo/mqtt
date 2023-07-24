1177014815456a7700342d4c0f7397bd0568bc0d
鲜帅 <2969280941@qq.com>
2023-07-24 10:32:45 +0800

    处理索引字符串的长度

--- src/main/java/com/NettyApplication/simulateclient/GroupChatClient.java
+++ src/main/java/com/NettyApplication/simulateclient/GroupChatClient.java
@@ -22,52 +22,58 @@ public class GroupChatClient {
         this.port = port;
     }
 
-    public void run() throws Exception{
+    public void run() throws Exception {
+        //2, 在 run() 方法中，创建一个 NioEventLoopGroup 对象作为事件循环组。
         EventLoopGroup group = new NioEventLoopGroup();
 
         try {
 
 
-        Bootstrap bootstrap = new Bootstrap()
-                .group(group)
-                .channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<SocketChannel>() {
+            Bootstrap bootstrap = new Bootstrap()
+                    .group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
 
-                    @Override
-                    protected void initChannel(SocketChannel ch) throws Exception {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
 
-                        //得到pipeline
-                        ChannelPipeline pipeline = ch.pipeline();
-                        //加入相关handler
-                        pipeline.addLast("decoder", new MyDecoder());
-                        pipeline.addLast("encoder", new StringEncoder());
-                        //加入自定义的handler
-                        pipeline.addLast(new GroupChatClientHandler());
-                    }
-                })
-                .option(ChannelOption.SO_KEEPALIVE, true); // 设置长连接;
+                            //得到pipeline
+                            ChannelPipeline pipeline = ch.pipeline();
+                            //加入相关handler
+                            pipeline.addLast("decoder", new MyDecoder());
+                            pipeline.addLast("encoder", new StringEncoder());
+                            //加入自定义的handler
+                            pipeline.addLast(new GroupChatClientHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_KEEPALIVE, true); // 设置长连接;
 
-        ChannelFuture channelFuture = bootstrap.connect(host, port).sync();
-        //得到channel
+            ChannelFuture channelFuture = bootstrap.connect(host, port).sync();
+            //得到channel
             Channel channel = channelFuture.channel();
-            System.out.println("通道ip和端口号-------" + channel.localAddress()+ "--------");
+            System.out.println("通道ip和端口号-------" + channel.localAddress() + "--------");
             //客户端需要输入信息，创建一个扫描器
             Scanner scanner = new Scanner(System.in);
-            while(scanner.hasNextLine()) {
+            while (scanner.hasNextLine()) {
                 String msg = scanner.nextLine();
                 //通过channel 发送到服务器端 AA01016400001FF
 //                byte[] msgBytes = {(byte) 0xAA, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, (byte) 0xFF};
-                byte[] msgBytes = new byte[msg.length()/2];
+                byte[] msgBytes = new byte[msg.length() / 2];
 
                 for (int i = 0; i < msg.length(); i += 2) {
-                    String substring = msg.substring(i, i + 2);
-                    int decimalValue = Integer.parseInt(substring, 16);
-                    byte byteValue = (byte) decimalValue;
-                    msgBytes[i/2] = byteValue;
+                    if (i + 2 <= msg.length()) {
+                        String substring = msg.substring(i, i + 2);
+                        int decimalValue = Integer.parseInt(substring, 16);
+                        byte byteValue = (byte) decimalValue;
+                        msgBytes[i / 2] = byteValue;
+                    } else {
+                        // 处理索引超出范围的情况
+                    }
+
                 }
                 channel.writeAndFlush(Unpooled.wrappedBuffer(msgBytes)).sync();
             }
-        }finally {
+        } finally {
             group.shutdownGracefully();
         }
     }
@@ -76,6 +82,7 @@ public class GroupChatClient {
 //        new GroupChatClient("127.0.0.1", 20000).run();
         //
 //        new GroupChatClient("316n689m11.zicp.fun", 12459).run();
+        //1, 创建 GroupChatClient 类，并设置服务器的主机名（host）和端口号（port）作为属性。
         new GroupChatClient("127.0.0.1", 20000).run();
     }
 }
