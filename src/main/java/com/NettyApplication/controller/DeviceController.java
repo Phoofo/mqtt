bc7e109ee3a0309129d08c69643d4d40ccbea76b
鲜帅 <2969280941@qq.com>
2023-07-26 17:10:08 +0800

    批量操作空调接口实现

--- src/main/java/com/NettyApplication/controller/DeviceController.java
+++ src/main/java/com/NettyApplication/controller/DeviceController.java
@@ -1,8 +1,11 @@
 package com.NettyApplication.controller;
 
 
+import com.NettyApplication.entity.DeviceInfo;
 import com.NettyApplication.entity.dto.AirOperationDto;
 import com.NettyApplication.listen.DtuManage;
+import com.NettyApplication.service.IDeviceInfoService;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,9 @@ import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -27,6 +33,8 @@ public class DeviceController {
 
     @Resource
     private DtuManage dtuManage;
+    @Resource
+    private IDeviceInfoService iDeviceInfoService;
 
     @Operation(description = "操作空调:/查询/开机/关机/制冷/制热/除湿")
     @PostMapping("/setAir")
@@ -52,6 +60,51 @@ public class DeviceController {
         short s = dto.getControlId();
         // 设置硬件的状态
         dtuManage.sendMsg(msgBytes, s);
+
+        return ResponseEntity.ok("Success!");
+    }
+
+    /**
+     * 批量操作空调:/查询/开机/关机/制冷/制热/除湿
+     *
+     * @param dto
+     * @return
+     */
+    @Operation(description = "批量操作空调:/查询/开机/关机/制冷/制热/除湿")
+    @PostMapping("/setAirBatch")
+    public ResponseEntity<String> setAirBatch(@RequestBody AirOperationDto dto) {
+        Assert.notNull(dto.getControlId(), "主板编号不能为空");
+        Assert.notNull(dto.getOperation(), "空调操作不能为空");
+        Assert.notNull(dto.getOperationType(), "操作类型不能为空");
+        Assert.notNull(dto.getDeviceTypeId(), "设备类型不能为空");
+        Assert.isTrue(dto.getDeviceTypeId() == 1L, "设备类型不为空调");
+
+        List<Byte> deviceIds = new ArrayList<>();
+        if (dto.getOperationType() == 1) {//批量操作
+            Assert.notNull(dto.getDeviceIds(), "设备编码集不能为空");
+            deviceIds = dto.getDeviceIds();
+        } else if (dto.getOperationType() == 2) {//一键操作
+            deviceIds = iDeviceInfoService.list(Wrappers.lambdaQuery(DeviceInfo.class)
+                    .eq(DeviceInfo::getControlId, dto.getControlId()))
+                    .stream().map(DeviceInfo::getDeviceId).collect(Collectors.toList());
+        }
+
+        deviceIds.forEach(deviceId -> {
+            // 报文封装
+            byte[] msgBytes = {
+                    (byte) Integer.parseInt("AA", 16),//开头
+                    (byte) Integer.parseInt("01", 16),//设备类型:空调01
+                    deviceId,//设备编号
+                    dto.getOperation(),//功能:01查询;02开机(自动);03关机;04制冷;05制热;06除湿
+                    (byte) Integer.parseInt("00", 16),//可拓展参数
+                    (byte) Integer.parseInt("00", 16),//可拓展参数
+                    (byte) Integer.parseInt("00", 16),//可拓展参数
+                    (byte) Integer.parseInt("FE", 16) //结尾
+            };
+            System.out.println(msgBytes);
+            // 设置硬件的状态
+            dtuManage.sendMsg(msgBytes, dto.getControlId());
+        });
 
         return ResponseEntity.ok("Success!");
     }
--- src/main/java/com/NettyApplication/entity/dto/AirOperationDto.java
+++ src/main/java/com/NettyApplication/entity/dto/AirOperationDto.java
@@ -2,6 +2,8 @@ package com.NettyApplication.entity.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class AirOperationDto {
 
@@ -9,6 +11,11 @@ public class AirOperationDto {
      * 主板编号
      */
     private short controlId;
+
+    /**
+     * 设备编号集合
+     */
+    private List<Byte> deviceIds;
 
     /**
      * 设备编号
@@ -19,6 +26,11 @@ public class AirOperationDto {
      * 设备类型ID
      */
     private byte deviceTypeId;
+
+    /**
+     * 操作方式 ：1,批量选择;2,主板一键
+     */
+    private Integer operationType;
 
     /**
      * 空调操作
